1fccfd90efbb79771d793fd963be8b9f09260de9
Vishal kumar <105039647+itsvishi@users.noreply.github.com>
2026-03-01 02:55:26 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import sys
 import cv2
 import pytesseract
 import numpy as np
@@ -17,7 +18,8 @@ from google.genai import types
 # 2. Tesseract OCR MUST be installed on your system.
 #    - Windows: https://github.com/UB-Mannheim/tesseract/wiki
 #    - You may need to specify the path below if it's not in your system PATH:
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+if sys.platform.startswith('win'):
+    pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 # 3. Poppler is required for `pdf2image` on Windows/Mac.
 #    - Windows: https://github.com/oschwartz10612/poppler-windows/releases/
 #    - Add poppler's bin folder to your system PATH.
